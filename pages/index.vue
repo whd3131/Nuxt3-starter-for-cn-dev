c6e2bf3377b55ad7993361b6d33c1e6b65b0409b
zuck <973944922@qq.com>
2024-02-24 10:19:12 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page-index">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Logos />
+    <Examples />
+  </div>
+</template>
+
+<style>
+.page-index {
+  padding-top: 60px;
+  text-align: center;
+}
+</style>
